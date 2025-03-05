1ecc9d3818929608938fa13b596887f78f3ceb67
rmens <r.mens@me.com>
2025-03-05 21:09:00 +0100

    And this

--- includes/class-cachemanager-queue.php
+++ includes/class-cachemanager-queue.php
@@ -6,23 +6,23 @@ if (! defined('ABSPATH')) {
 namespace ZW_CACHEMAN_Core;
 
 /**
- * Class CacheManager_Queue
+ * Class CacheManagerQueue
  *
  * Handles queuing of URLs for low-priority processing.
  */
-class CacheManager_Queue
+class CacheManagerQueue
 {
     /**
      * Singleton instance.
      *
-     * @var CacheManager_Queue
+     * @var CacheManagerQueue
      */
     private static $instance;
 
     /**
      * Returns the singleton instance.
      *
-     * @return CacheManager_Queue
+     * @return CacheManagerQueue
      */
     public static function get_instance()
     {
@@ -40,7 +40,7 @@ class CacheManager_Queue
     public function queue_low_priority_urls($urls)
     {
         $existing_urls = get_option(ZW_CACHEMAN_LOW_PRIORITY_STORE, []);
-        $all_urls = array_unique(array_merge($existing_urls, $urls));
+        $all_urls      = array_unique(array_merge($existing_urls, $urls));
         update_option(ZW_CACHEMAN_LOW_PRIORITY_STORE, $all_urls);
     }
 }
