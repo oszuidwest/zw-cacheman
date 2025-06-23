e27ed0ab76e55e5d702e0ac08e1b350bdebcc3ea
Raymon Mens <raymon@zuidwestfm.nl>
2025-06-23 21:06:44 +0200

    chore: update version

--- zuidwest-cache-manager.php
+++ zuidwest-cache-manager.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: ZuidWest Cache Manager
  * Description: Purges Cloudflare cache for high-priority URLs when posts are published, edited, or deleted. It also queues related taxonomy URLs for low-priority batch processing via WP-Cron.
- * Version: 1.5
+ * Version: 1.5.1
  * Author: Streekomroep ZuidWest
  * License: GPLv3
  * Text Domain: zw-cacheman
